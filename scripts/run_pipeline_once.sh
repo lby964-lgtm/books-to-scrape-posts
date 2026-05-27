58a47f26c6505debea0fcd69fe3fedcc660fcfdc
lby964-lgtm <lby964@gmail.com>
2026-05-27 15:04:07 +0900

    Allow fixed post date in pipeline

--- scripts/run_pipeline_once.sh
+++ scripts/run_pipeline_once.sh
@@ -6,6 +6,7 @@ cd "$ROOT_DIR"
 
 COUNT="${COUNT:-5}"
 PYTHON_BIN="${PYTHON_BIN:-python}"
+POST_DATE="${POST_DATE:-$(date +%F)}"
 
 "$PYTHON_BIN" src/crawl_books.py --count "$COUNT" --output data/books.json
-"$PYTHON_BIN" src/generate_markdown.py --input data/books.json --output-dir posts
+"$PYTHON_BIN" src/generate_markdown.py --input data/books.json --output-dir posts --date "$POST_DATE"
